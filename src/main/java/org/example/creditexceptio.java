431a6a4ab89f0a151ef62018afe8be4d95b0217c
nbhaargavi <123439300+nbhaargavi@users.noreply.github.com>
2023-01-31 15:58:47 +0530

    Update creditexceptio.java

--- src/main/java/org/example/creditexceptio.java
+++ src/main/java/org/example/creditexceptio.java
@@ -1,15 +1,14 @@
 package org.example;
 import java.util.Scanner;
-import java.util.logging.*;
 import java.util.logging.Logger;
-public class creditexceptio implements Cloneable {
+public class Creditexceptio implements Cloneable {
     Logger l=Logger.getLogger("kitty");
     private String cname;
     private Integer cno;
     private Integer cnum = 501005463;
     private Integer expdate;
 
-    public creditexceptio( String cname,Integer cno, Integer expdate) {
+    public Creditexceptio( String cname,Integer cno, Integer expdate) {
         this.cname=cname;
         this.cno=cno;
         this.expdate=expdate;
@@ -24,11 +23,11 @@ public class creditexceptio implements Cloneable {
             l.info("OOPS! Credit card number doesn't match");
         }
     }
-    creditexceptio cr()
+    Creditexceptio cr()
     {
         try
         {
-            return (creditexceptio) super.clone();
+            return (Creditexceptio) super.clone();
         }
         catch(CloneNotSupportedException e)
         {
@@ -46,8 +45,8 @@ public class creditexceptio implements Cloneable {
         Integer cno = sc.nextInt();
         l.info("Enter the expiration date:");
         Integer expdate = sc.nextInt();
-        creditexceptio c1=new creditexceptio(cname,cno,expdate);
-        creditexceptio c2 = c1.cr();
+        Creditexceptio c1=new Creditexceptio(cname,cno,expdate);
+        Creditexceptio c2 = c1.cr();
         c2.eq();
     }
 }
